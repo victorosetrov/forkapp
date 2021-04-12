3023ca413aed538f46969446012752fc11c66b9d
Viktor <victorosetrov@github.com>
2021-04-12 18:18:32 +0100

    Completed project.

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e84e0980-8ef1-4e18-87f2-1a07709532ea';
+export const MODAL_CLOSE_SEC = 2.5;
